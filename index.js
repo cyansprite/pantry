8c63619981df0c1baf244a3085227a788f9c32d2
Jojo Brandon Coffman <brandon.coffman@lexmark.com>
2020-10-09 16:59:15 -0700

    Init Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/** Required External Modules */
+const express = require("express");
+const path = require("path");
+
+/** App Variables */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/** App Configuration */
+app.use(express.static(path.join(__dirname, "public")));
+
+/** Routes Definitions */
+app.get("/", (req, res) => {
+    console.log(`Get for /`);
+});
+
+/** Server Activation */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
